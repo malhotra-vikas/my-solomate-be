58f02277759cb4dc0794bd269b54e8423dd6c0e9
Nikunj1896 <nikunj.projects9893@gmail.com>
2025-07-28 17:42:46 +0530

    Added subscription api

---
+++ app/api/subscriptions/create-checkout-session/route.ts
@@ -0,0 +1,30 @@
+import Stripe from 'stripe';
+import { NextRequest, NextResponse } from 'next/server';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-06-30.basil",
+});
+  
+
+export async function POST(req: NextRequest) {
+
+  const { userId, priceId, tier } = await req.json()
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'subscription',
+      line_items: [{ price: priceId, quantity: 1 }],
+      payment_method_types: ['card'],
+      success_url: `mysolomate://success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: 'mysolomate://cancel',
+      metadata: { userId, tier },
+    });
+    
+    console.log("🚀 ~ POST ~ session:", session)
+
+    return NextResponse.json({ url: session.url }, { status: 200 })
+  } catch (error: any) {
+    // res.status(500).json({ error: error.message });
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
