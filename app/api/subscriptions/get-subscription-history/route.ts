7ff7527d94848b094bd6c552b76e184f5bf16573
Nikunj1896 <nikunj.projects9893@gmail.com>
2025-07-30 11:13:40 +0530

    Added price in history api

--- app/api/subscriptions/get-subscription-history/route.ts
+++ app/api/subscriptions/get-subscription-history/route.ts
@@ -1,6 +1,11 @@
 import { getUserIdFromRequest } from "@/lib/extractUserFromRequest";
 import { createClient } from "@/lib/supabase";
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-06-30.basil",
+});
 
 export async function GET(req: NextRequest) {
   const userId = await getUserIdFromRequest(req);
@@ -14,20 +19,55 @@ export async function GET(req: NextRequest) {
   try {
     const supabase = createClient();
 
-    const { data, error } = await supabase
+    const { data: subscriptions, error } = await supabase
       .from("subscriptions")
       .select("*")
       .eq("user_id", userId)
       .order("subscription_start_date", { ascending: false });
 
-    if (error || !data) {
+    if (error || !subscriptions) {
       return NextResponse.json(
         { error: "Subscription History was not found" },
         { status: 401 }
       );
     }
 
-    return NextResponse.json(data, { status: 200 });
+    const enriched = await Promise.all(
+      subscriptions.map(async (sub) => {
+        if (!sub.stripe_subscription_id || sub.tier === "free") {
+          return {
+            ...sub,
+            price: 0,
+            currency: "USD",
+          };
+        }
+
+        try {
+          const stripeSub = await stripe.subscriptions.retrieve(
+            sub.stripe_subscription_id
+          );
+
+          const price = stripeSub.items?.data[0]?.price;
+          const amount = price?.unit_amount ?? 0;
+          const currency = price?.currency?.toUpperCase() ?? "USD";
+
+          return {
+            ...sub,
+            price: amount / 100,
+            currency,
+          };
+        } catch (err) {
+          console.error("Stripe subscription fetch failed:", err);
+          return {
+            ...sub,
+            price: null,
+            currency: null,
+          };
+        }
+      })
+    );
+
+    return NextResponse.json({data: enriched}, { status: 200 });
   } catch (error) {
     console.error("Error while getting subscription history:", error?.message)
     return NextResponse.json({ error: "Internal server error" }, { status: 500 })
